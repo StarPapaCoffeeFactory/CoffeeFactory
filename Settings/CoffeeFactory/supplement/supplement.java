88013d1cc7fe7f9bba9b28ddfc203af0d7f329b1
codFish3 <72798393+codFish3@users.noreply.github.com>
2021-10-14 21:55:25 +0800

    update

--- Settings/CoffeeFactory/supplement/supplement.java
+++ Settings/CoffeeFactory/supplement/supplement.java
@@ -5,24 +5,20 @@ import Settings.CoffeeFactory.personnel.visitlimit.LimitContext;
 
 public abstract class supplement {
 
-    protected String model;//型号
+    //protected String model;//型号
     protected String id;//编号
+    protected String type;//类别
 
     protected Area location;//当前所在区域
     protected LimitContext visitLimit;//可用区域限制
 
-    public supplement(String model,String id) {
-        this.model=model;
-        this.id=id;
+    public supplement() {
+        //this.model=model;
+        //this.id=id;
+        this.type="abstractSupplement";
     }
 
-    public void setModel(String model) {
-        this.model=model;
-    }
 
-    public String getModel() {
-        return this.model;
-    }
 
     public String toString() {
         return "abstract class Personnel";
