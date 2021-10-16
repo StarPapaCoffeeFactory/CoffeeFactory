a08684242083b133bf77b84decd84e97b2f0443c
hky011011 <62506784+hky011011@users.noreply.github.com>
2021-10-16 20:47:04 +0800

    update out form

--- Test/DesignPatternTests/MementoTest.java
+++ Test/DesignPatternTests/MementoTest.java
@@ -4,61 +4,57 @@ import java.util.Scanner;
 
 public class MementoTest {
     public static void main(String[] args) {
+        //TODO：相关场景
         System.out.println("------------------------------------ [Memento] Test ------------------------------------");
-
         System.out.println("");
-        System.out.println("Product : createMemento() : create a memento for the product to store message of weight of single item");
+        System.out.println("Product : createMemento() : create a memento for the product to store message of quantity of single item");
         System.out.println("Product : restoreMemento(memento : Memento) : restore message from the memento");
-        System.out.println("ProductMemento : setWeight(weight : int) : set the weight of product in the memento");
-        System.out.println("ProductMemento : getWeight() : get the weight of product in the memento");
+        System.out.println("ProductMemento : setWeight(quantity : int) : set the quantity of product in the memento");
+        System.out.println("ProductMemento : getWeight() : get the quantity of product in the memento");
         System.out.println("ProductCaretaker : setProductMemento(productMemento : ProductMemento） : " +
                 "set the concrete memento in the product caretaker");
         System.out.println("ProductCaretaker : getProductMemento() : get the concrete product memento");
         System.out.println("");
-
+        //测试流程
         System.out.println("");
-        System.out.println("**************************** Memento Test *****************************");
-        System.out.println("***                          1. Set the first weight of product     ***");
-        System.out.println("***                          2. Store it in a memento               ***");
-        System.out.println("***                          3. Set a new weight of product         ***");
-        System.out.println("***                          4. Get the old weight from memento     ***");
-        System.out.println("***                                                                 ***");
-        System.out.println("*** Tips: This test doesn't need to input order,                    ***");
-        System.out.println("***       and you just need to follow the steps.                    ***");
-        System.out.println("***********************************************************************");
+        System.out.println("┌─────────────────────────   Memento Test   ─────────────────────────┐");
+        System.out.println("│              1. Set the initial quantity of products               │");
+        System.out.println("│              2. Store the quantity in the memento                  │");
+        System.out.println("│              3. Set new quantity of products                       │");
+        System.out.println("│              4. Get the old quantity from memento                  │");
+        System.out.println("└────────────────────────────────────────────────────────────────────┘");
         System.out.println("");
-
+        //第一步：设置产品的初始数量
         Scanner input = new Scanner(System.in);
-        int weight;
-
+        int quantity;
         System.out.println("Let's make a chocolate cake!");
         System.out.print("Step 1: Input the size of the chocolate cake" +
-                "(weight = 100*size*size):");
-        weight = input.nextInt();
+                "(quantity = 100*size*size):");
+        quantity = input.nextInt();
 
-        ChocolateCake cake = new ChocolateCake(weight);
+        ChocolateCake cake = new ChocolateCake(quantity);
         System.out.println("");
         System.out.println("Successfully create a chocolate cake of " + cake.getWeight() + " grams");
         System.out.println("");
 
-        System.out.println("Step 2: Create a memento to store the weight of the chocolate cake");
+        System.out.println("Step 2: Create a memento to store the quantity of the chocolate cake");
         System.out.println("Create a product memento...");
         ProductCaretaker caretaker = new ProductCaretaker();
         caretaker.setProductMemento(cake.createMemento());
-        System.out.println("Message in the memento:    The weight of chocolate cake in the memento is " +
+        System.out.println("Message in the memento:    The quantity of chocolate cake in the memento is " +
                 caretaker.getProductMemento().getWeight() + " grams.");
         System.out.println("");
 
-        System.out.println("Step 3: Set a new weight of the chocolate cake");
-        System.out.print("Input the new weight:");
-        weight = input.nextInt();
-        cake.setWeight(weight);
+        System.out.println("Step 3: Set a new quantity of the chocolate cake");
+        System.out.print("Input the new quantity:");
+        quantity = input.nextInt();
+        cake.setWeight(quantity);
         System.out.println("");
-        System.out.println("Now the weight of chocolate cake is " + cake.getWeight() + " grams");
+        System.out.println("Now the quantity of chocolate cake is " + cake.getWeight() + " grams");
         System.out.println("");
 
-        System.out.println("Step 4: Get the old weight from memento");
+        System.out.println("Step 4: Get the old quantity from memento");
         cake.restoreMemento(caretaker.getProductMemento());
-        System.out.println("Now the weight of chocolate cake is " + cake.getWeight() + " grams");
+        System.out.println("Now the quantity of chocolate cake is " + cake.getWeight() + " grams");
     }
 }
