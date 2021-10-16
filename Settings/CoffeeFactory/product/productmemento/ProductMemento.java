371400aa02706ff71644918ccdb5151206ff9188
hky011011 <62506784+hky011011@users.noreply.github.com>
2021-10-16 18:19:13 +0800

    create productmemento

---
+++ Settings/CoffeeFactory/product/productmemento/ProductMemento.java
@@ -0,0 +1,18 @@
+package Settings.CoffeeFactory.product.productmemento;
+
+public class ProductMemento {
+
+    private int quantity;
+
+    public ProductMemento(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
