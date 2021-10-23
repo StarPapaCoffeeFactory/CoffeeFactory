6041e574a3a3273dbbca1e95d0e86c5d297902ba
zjx-forever <1098505411@qq.com>
2021-10-23 16:22:55 +0800

    1

--- Settings/CoffeeFactory/areas/PublicArea/ExperienceRoom.java
+++ Settings/CoffeeFactory/areas/PublicArea/ExperienceRoom.java
@@ -69,6 +69,7 @@ public class ExperienceRoom extends PublicArea {
      * Visitor Pattern
      *
      */
+
     public void accept(Visitor v) {
         v.visit(this);
     }
---
+++ Test/DesignPatternTests/VisitorTest.java
@@ -0,0 +1,19 @@
+package Test.DesignPatternTests;
+
+public class VisitorTest {
+    public VisitorTest() {
+    }
+
+    public static void main(String[] args) {
+
+        //System.out.println("");
+        System.out.println("┌──────────────────────────────────────   Visitor Test   ────────────────────────────────┐");
+        System.out.println("│              1. Produce the product: Set the product to produced stage               │");
+        System.out.println("│              2. Pack the product: Set the product to packed stage                    │");
+        System.out.println("│              3. Store the product: Set the product to Stored stage                   │");
+        System.out.println("└──────────────────────────────────────────────────────────────────────────────────────┘");
+        System.out.println("");
+
+        System.out.println("ddd");
+    }
+}
