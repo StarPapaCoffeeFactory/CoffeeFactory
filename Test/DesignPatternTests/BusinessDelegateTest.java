b88c246a292684cf6733f87b534dd797948458fb
zjx-forever <1098505411@qq.com>
2021-10-25 17:00:45 +0800

    1

--- Test/DesignPatternTests/BusinessDelegateTest.java
+++ Test/DesignPatternTests/BusinessDelegateTest.java
@@ -1,5 +1,10 @@
 package Test.DesignPatternTests;
 
+import Settings.CoffeeFactory.areas.ManufacturingArea.Warehouse.Delegate.WarehouseClient;
+import Settings.CoffeeFactory.areas.ManufacturingArea.Warehouse.Delegate.WarehouseDelegate;
+import Settings.CoffeeFactory.areas.ManufacturingArea.Warehouse.Warehouse;
+import Settings.CoffeeFactory.material.Food.Milk;
+
 import java.util.Scanner;
 
 public class BusinessDelegateTest {
@@ -9,10 +14,20 @@ public class BusinessDelegateTest {
         System.out.println("");
         System.out.println("┌─────────────────────────   Business Delegate Test   ──────────────────────────┐");
         System.out.println("│               0. Exit!!!                                                      │");
-        System.out.println("│               1. Testing the get material service                             │");
-        System.out.println("│               2. Testing the storage material service                         │");
+        System.out.println("│               1. Testing the storage material service                         │");
+        System.out.println("│               2. Testing the get material service                             │");
         System.out.println("└───────────────────────────────────────────────────────────────────────────────┘");
         System.out.println("");
+
+        WarehouseDelegate warehouseDelegate;
+        WarehouseClient warehouseClient;
+
+        //Create a new warehouse
+        Warehouse warehouse = new Warehouse();
+        //Create a new warehouse delegate
+        warehouseDelegate = new WarehouseDelegate(warehouse);
+        //Create a new warehouse client
+        warehouseClient = new WarehouseClient(warehouseDelegate);
 
         Scanner input = new Scanner(System.in);
         int op;
@@ -32,11 +47,14 @@ public class BusinessDelegateTest {
                 case 1:
                 {
 
+                    warehouseDelegate.setServiceType("StorageMaterial");
+                    warehouseDelegate.doTask(new Milk());
                     break;
                 }
                 case 2:
                 {
-
+                    warehouseDelegate.setServiceType("GetMaterial");
+                    warehouseDelegate.doTask(new Milk());
                     break;
                 }
 
