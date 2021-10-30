29a411e63c9e402f9721bb7011c43943c1688e9c
Davis Wang <zgjshawl@gmail.com>
2021-10-30 11:44:10 +0800

    restore screen class
    restore screen class to satisfy the requirements of the Monostate Test Pattern.plz don't delete the class

---
+++ Settings/CoffeeFactory/supplement/screen/screen.java
@@ -0,0 +1,23 @@
+package Settings.CoffeeFactory.supplement.screen;
+
+import Settings.CoffeeFactory.supplement.supplement;
+
+public class screen extends supplement {
+    private static String context = "Hello! StarpapaCoffee!";//屏幕内容
+    public screen() {
+        this.type = "screen";
+    }
+
+    public void setContent(String context) {
+        this.context = context;
+    }
+    public String getContext(){
+        return this.context;
+    }
+
+    @Override
+    public String toString() {
+        return "class Visitor extends Person";
+    }
+
+}
